da4fcd392fce34ef522766321985f1d120d54d7a
hi20160616 <hi20160616@163.com>
2021-10-13 12:46:27 +0800

    fix wrong return on fetcher.Get

--- internal/fetcher/article.go
+++ internal/fetcher/article.go
@@ -56,8 +56,8 @@ func (a *Article) Get(id string) (*Article, error) {
 			return a, nil
 		}
 	}
-	return nil, fmt.Errorf("[%s] no article with id: %s, url: %s",
-		configs.Data.MS["voa"].Title, id, a.U.String())
+	return nil, fmt.Errorf("[%s] no article with id: %s",
+		configs.Data.MS["voa"].Title, id)
 }
 
 func (a *Article) Search(keyword ...string) ([]*Article, error) {
